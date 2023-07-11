13c91f3d3596f6c03de4bce87af16dccaa9efcd7
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-07-11 12:36:50 +0800

    Test function to add one cell to display table

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function displayLibrary() {
+    myLibrary.forEach((book) => {
+        let newRow = document.createElement("tr");
+        let authorElement = document.createElement("td");
+        authorElement.textContent = `${book.author}`
+        newRow.appendChild(authorElement)
+        tableBody.appendChild(newRow)
+    });
+}
+
 let book1 = new Book("The Hobbit", "J.R.R. Martin", 500, "read");
 
 addBookToLibrary(book1);
+
+let tableBody = document.querySelector(".table-body")
+
+displayLibrary();
